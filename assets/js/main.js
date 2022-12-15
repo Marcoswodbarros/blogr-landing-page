609e565440a5f950e0348b62a2165ec4db51fed5
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2022-12-15 09:22:55 -0300

    Starting the desktop version
    - Pictures and backgrounds.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 var leftButton = document.querySelector('.john__button--left');
 var rightButton = document.querySelector('.tanya__button--right');
 
-var tanyaContainer = document.querySelector(".container__tanya");
-var johnContainer = document.querySelector('.container__john');
+var tanyaContainer = document.querySelector(".section__container--tanya");
+var johnContainer = document.querySelector('.section__container--john');
 
 rightButton.addEventListener('click', function (){
     tanyaContainer.style.display = "none";
